d73e4cf5c7a2023b4d79403bcc17e5ce027c1b0a
sumrahfatima <fatima14sumrah@gmail.com>
2020-07-19 14:00:40 +0530

    index migrations  error

--- index/views.py
+++ index/views.py
@@ -7,4 +7,12 @@ def index(request):
     return render(request, 'index/index.html', {'message' : message})
 
 def auth(request):
-        return render(request, 'profill/profill.html')
+    user = request.POST.get('user')
+    password = request.POST.get('password')
+    try:
+        authName = User.objects.get(user_name=user, user_password=password).user_name
+        print(authName)
+        return render(request,'profill/profill.html')
+    except:
+        message = "Authentication Failed!! Wrong username or password"
+        return render(request, 'index/index.html', {'message': message})
