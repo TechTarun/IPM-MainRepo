c530acbb23cd89c4eb4f03c7a61d102275cf1ae0
sumrahfatima <fatima14sumrah@gmail.com>
2020-07-20 13:37:22 +0530

    updated ui/ux

--- index/views.py
+++ index/views.py
@@ -7,12 +7,5 @@ def index(request):
     return render(request, 'index/index.html', {'message' : message})
 
 def auth(request):
-    user = request.POST.get('user')
-    password = request.POST.get('password')
-    try:
-        authName = User.objects.get(user_name=user, user_password=password).user_name
-        print(authName)
-        return render(request,'profill/profill.html')
-    except:
-        message = "Authentication Failed!! Wrong username or password"
-        return render(request, 'index/index.html', {'message': message})
+
+    return render(request,'profill/profill.html')
